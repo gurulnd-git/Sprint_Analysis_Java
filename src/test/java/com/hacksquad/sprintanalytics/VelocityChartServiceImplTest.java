376ee0adfa834bf339bb28d14c97291356d3e54f
Akash Bhasy <U49583@UST-INVLAB.COM>
2019-08-10 14:46:43 +0530

    added test case

---
+++ src/test/java/com/hacksquad/sprintanalytics/VelocityChartServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.hacksquad.sprintanalytics;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class VelocityChartServiceImplTest {
+    @Test
+    public void TestImpl() {
+        // TODO Auto-generated method stub
+        // Sample tester = new Sample(); // MyClass is tested
+        // assert statements
+        String S1= "hello world";
+       // assertEquals(S1,tester.Hello());
+    }
+
+}
